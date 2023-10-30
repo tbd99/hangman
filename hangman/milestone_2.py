2034f7ca3e951c4602f28680a746a5e704500ecd
Tiger Davies <tigerdavies@Tigers-Air.cable.virginm.net>
2023-10-30 12:51:36 +0000

    Asks the user for an input and prints their input

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -8,3 +8,7 @@ print(word_list)
 
 word = random.choice(word_list) #generates random word from given list
 print(word)
+
+#task 3 - ask the user for an input 
+guess = input("Please enter a single letter ")
+print(guess)
