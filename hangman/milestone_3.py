00d5106f69a74ab95595fea94dd7c45aa5a44f9f
Tiger Davies <tigerdavies@Tigers-Air.cable.virginm.net>
2023-10-30 17:12:48 +0000

    Updates the code to be clearer with optimised comments and layout

--- hangman/milestone_3.py
+++ hangman/milestone_3.py
@@ -1,21 +1,21 @@
-#code to iteratively check if the input is a valid guess 
+
 import random 
 
-fruit_list = ['appple','pear','mango','peach','pineapple','peach'] #defines list of fruits
-random_fruit = random.choice(fruit_list) #generates random word from given list
-#print(random_fruit) #sensecheck
+fruit_list = ['appple','pear','mango','peach','pineapple','peach'] #defines list of fruit, word to guess is chosen from here
+random_fruit = random.choice(fruit_list) #generates random word from fruit list, the word to be guessed
 
-#define function to check if the guessed letter is in a word 
-def check_guess(letter_guess): #pass guessed letter and random word as arguments
+
+
+def check_guess(letter_guess): #def function to check if the guessed letter is in a word, pass guessed letter as arg
     letter_guess = letter_guess.lower() #convert guess to lower case 
-    if letter_guess in random_fruit: #checks if the guessed letter is in the word, defined outside the function
+    if letter_guess in random_fruit: #checks if the guessed letter is in the word (defined outside the function)
        print(f"Good guess! {letter_guess} is in the word.") #prints if the letter is in the word
     else:
        print(f"Sorry, {letter_guess} is not in the word. Try again.") #prints if the letter is not in the word
     return 
 
-#define function to get user input
-def ask_for_input():
+
+def ask_for_input(): #define function to get user input
    while 1 < 2: #creates infinite while loop
     letter_guess = input("Please enter a single letter ") #user input of letter guess 
     if len(list(letter_guess)) == 1 and letter_guess.isalpha(): #breaks out the loop if the letter guess is a single alphabetical character
@@ -23,12 +23,12 @@ def ask_for_input():
     else:
         print('Invalid letter. Please, enter a single alphabetical character') #otherwise prints this message and while loop continues until a valid input is given 
 
-   check_guess(letter_guess)
-   return letter_guess
+   check_guess(letter_guess) #calls check_guess function within this function 
+   return letter_guess #returns guessed letter
 
 
 
-ask_for_input() #call function
+ask_for_input() #calls function defined above
 
 
 
