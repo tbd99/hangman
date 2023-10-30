dda16a29ee421b6031c287f1f6e5d5f551bae9ad
Tiger Davies <tigerdavies@Tigers-Air.cable.virginm.net>
2023-10-30 12:50:32 +0000

    Defines list of fruits and selects a random word from the list

---
+++ hangman/milestone_2.py
@@ -0,0 +1,10 @@
+#task 1 - define the list of possible words 
+#task 2 - choose a random word from the list
+
+import random 
+
+word_list = ['appple','pear','mango','peach','pineapple'] #defines list of fruits
+print(word_list)
+
+word = random.choice(word_list) #generates random word from given list
+print(word)
