4b98ebac43c68bfb21f8d266663c0662d7975925
Tiger Davies <tigerdavies@Tigers-Air.cable.virginm.net>
2023-10-30 16:37:28 +0000

    Creates the function check_guess to check if the guessed letter is in the word

--- hangman/milestone_3.py
+++ hangman/milestone_3.py
@@ -1,5 +1,17 @@
 #code to iteratively check if the input is a valid guess 
 
+#define function to check if the guessed letter is in a word 
+def check_guess(letter_guess,random_word):
+    letter_guess = letter_guess.lower() #convert guess to lower case 
+    if letter_guess in random_word: #checks if the guessed letter is in the word
+       print(f"Good guess! {letter_guess} is in the word.") #prints if the letter is in the word
+    else:
+       print(f"Sorry, {letter_guess} is not in the word. Try again.") #prints if the letter is not in the word
+    return 
+
+
+
+
 while 1 < 2: #creates infinite while loop
     letter_guess = input("Please enter a single letter ") #user input of letter guess 
     if len(list(letter_guess)) == 1 and letter_guess.isalpha(): #breaks out the loop if the letter guess is a single alphabetical character
@@ -15,7 +27,5 @@ fruit_list = ['appple','pear','mango','peach','pineapple'] #defines list of frui
 random_fruit = random.choice(fruit_list) #generates random word from given list
 #print(random_fruit)
 
-if letter_guess in random_fruit: #checks if the guessed letter is in the word
-    print(f"Good guess! {letter_guess} is in the word.") #prints if the letter is in the word
-else:
-    print(f"Sorry, {letter_guess} is not in the word. Try again.") #prints if the letter is not in the word
+check_guess(letter_guess,random_fruit)
+
