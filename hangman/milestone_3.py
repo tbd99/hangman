72956a7e25dc020d4b4fedc8b6a208baf537438e
Tiger Davies <tigerdavies@Tigers-Air.cable.virginm.net>
2023-10-30 14:53:18 +0000

    Creates a while loop for checking if the user input guess is valid

---
+++ hangman/milestone_3.py
@@ -0,0 +1,8 @@
+#code to iteratively check if the input is a valid guess 
+
+while 1 < 2: #creates infinite while loop
+    letter_guess = input("Please enter a single letter ") #user input of letter guess 
+    if len(list(letter_guess)) == 1 and letter_guess.isalpha(): #breaks out the loop if the letter guess is a single alphabetical character
+        break 
+    else:
+        print('Invalid letter. Please, enter a single alphabetical character') #otherwise prints this message and while loop continues until a valid input is given 
