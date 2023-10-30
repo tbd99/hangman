d9033af9fb5ba075957206d94561001101dd8607
Tiger Davies <tigerdavies@Tigers-Air.cable.virginm.net>
2023-10-30 15:08:52 +0000

    Adds if statement to check if the guessed letter is in the generated word

--- hangman/milestone_3.py
+++ hangman/milestone_3.py
@@ -6,9 +6,16 @@ while 1 < 2: #creates infinite while loop
         break 
     else:
         print('Invalid letter. Please, enter a single alphabetical character') #otherwise prints this message and while loop continues until a valid input is given 
-import random 
 
+import random 
+print(letter_guess)
 fruit_list = ['appple','pear','mango','peach','pineapple'] #defines list of fruits
-print(fruit_list)
+#print(fruit_list)
 
 random_fruit = random.choice(fruit_list) #generates random word from given list
+#print(random_fruit)
+
+if letter_guess in random_fruit: #checks if the guessed letter is in the word
+    print(f"Good guess! {letter_guess} is in the word.") #prints if the letter is in the word
+else:
+    print(f"Sorry, {letter_guess} is not in the word. Try again.") #prints if the letter is not in the word
