d83c2f544e75a3af3b0b1748394c191706f89d44
Tiger Davies <tigerdavies@Tigers-Air.cable.virginm.net>
2023-10-31 10:54:01 +0000

    Adds check_guess method to Hangman class, this is taken from check_guess function defined in milestone_3.py

--- hangman/milestone_4.py
+++ hangman/milestone_4.py
@@ -11,6 +11,14 @@ class Hangman():
         self.num_lives = num_lives #defines no of lives
         fruit_list.self = fruit_list #defines list of fruits
         self.list_of_guesses = [] #initialise empty list
+    
+    def check_guess(letter_guess): #def method to check if the guessed letter is in a word, pass guessed letter as arg
+        letter_guess = letter_guess.lower() #convert guess to lower case 
+        if letter_guess in random_fruit: #checks if the guessed letter is in the word (defined outside the function)
+           print(f"Good guess! {letter_guess} is in the word.") #prints if the letter is in the word
+        else:
+           print(f"Sorry, {letter_guess} is not in the word. Try again.") #prints if the letter is not in the word
+        return 
  
 
 
